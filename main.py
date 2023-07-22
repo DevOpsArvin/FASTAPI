0cfbbaafa662c7ef6abf7dd94c7e5998c9330c5b
arvinpenaacosta <117697006+arvinpenaacosta@users.noreply.github.com>
2023-07-22 10:09:36 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,526 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+from fastapi.responses import RedirectResponse
+from fastapi import Response
+
+from contextlib import closing
+
+from fastapi import Response
+
+import sqlite3
+
+#import textwrap
+#import html
+
+
+import netmiko
+from netmiko.ssh_exception import NetmikoTimeoutException, SSHException, AuthenticationException
+from netmiko import ConnectHandler  # Import the ConnectHandler class
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"))
+
+# Initialize templates
+templates = Jinja2Templates(directory="templates")
+
+UsersLoggedIn = {}  # Define the UsersLoggedIn dictionary
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Connect to the SQLite database using a context manager
+def get_database_connection():
+    conn = sqlite3.connect("epmap.db")
+    return conn
+
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class User:
+    def __init__(self, username, password, hostname):
+        self.username = username
+        self.password = password
+        self.hostname = hostname
+
+    def cisco(self):
+        cisco = {
+            'device_type': 'cisco_ios',
+            'username': f'{self.username}',
+            'password': f"{self.password}",
+            'host': f'{self.hostname}'
+        }
+        return cisco
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class NetConnection:
+    def connect(self, cisco):
+        try:
+            self.net_connect = ConnectHandler(**cisco)  # Initialize net_connect as an instance attribute
+            result = "Success"
+
+
+        #except AuthenticationException as err:
+        #    error = err
+        #    print(error)
+        #    return err
+
+        except (AuthenticationException):
+            err = "Incorrect username or password"
+        #   print(result)  # Print the result
+            return err
+
+        except (NetmikoTimeoutException):
+            result = "Timeout to device"
+            #print(result)  # Print the result
+
+        except (EOFError):
+            result = "End of file while attempting device"
+            #print(result)  # Print the result
+
+        except SSHException as err:
+            error = err
+            #print(error)
+            return err
+
+        #except(SSHException):
+        #    result = "SSH Issue. Are you sure SSH is enabled?"
+        #    print(result)  # Print the result
+
+        except Exception as unknown_error:
+            result = f"Some other error: {unknown_error}"
+            #print(result)  # Print the result
+
+
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class TokenAccount:
+    instances = {}
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.instances[username] = self
+
+    def get_account(self, username):
+        return self.instances.get(username)
+
+    def create_account(self, username, password):
+        return TokenAccount(username, password)
+
+    def delete_account(self, username):
+        if username in self.instances:
+            del self.instances[username]
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def perform_search(query):
+    conn = sqlite3.connect("epmap.db")
+    cursor = conn.cursor()
+
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return results
+
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def get_mapping_results(station):
+    query = f"SELECT * FROM mapping WHERE station LIKE '%{station}%'"
+    results = perform_search(query)
+    return results
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def get_vlan_results():
+    query = f"SELECT * FROM vlans"
+    results = perform_search(query)
+    return results
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def get_voice_results():
+    query = f"SELECT * FROM voices"
+    results = perform_search(query)
+    return results
+
+
+#===================================================================
+@app.get("/")
+async def home(request: Request):
+    print("+++++++++++++++++++++++++++++++++++")
+    print(f"Number of User Logged-in to the System : {len(UsersLoggedIn)}")
+    print("+  User is preparing to Login...  +")
+    print("+++++++++++++++++++++++++++++++++++")
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+userValid = False
+
+#===================================================================
+@app.post("/login")
+async def process_login(
+        request: Request, 
+        username: str = Form(...), 
+        password: str = Form(...)
+        ):
+
+    if (username == "aa" and password == "aa") or (username == "bb" and password == "bb") or (username == "cc" and password == "cc"):
+        userValid = True
+        connection_result = None
+    else:    
+        userValid = False
+        connection_result = "ERROR : Username & Password.."
+
+    err = None
+            
+    print(f"360: {userValid}")
+    print(f"361: {connection_result}")
+
+
+    if userValid == True:
+
+        # Add the logged-in user to UsersLoggedIn dictionary
+        UsersLoggedIn[username] = TokenAccount(username, password)
+
+        # Print all account instances
+        for account_instance in TokenAccount.instances.values():
+            print(f"73=======: {account_instance.username} {account_instance.password}")
+
+            loginUsr = f"Account {account_instance.username} is accessing to Login... +"
+            print(f"+  {loginUsr}")
+            print(f"Number of User Logged-in to the System : {len(UsersLoggedIn)}")
+
+        account_instance = UsersLoggedIn.get(username)
+        if account_instance:
+            password = account_instance.password
+            print(f"368 =======: {username} {password}")
+            print(f"User :{username} STATUS: LOGGED IN")
+            
+        else:
+            print("Account not found.")
+
+
+        print(f"374 -- Error : {err}") 
+
+        return templates.TemplateResponse(
+            "search.html", 
+            {   "request": request, 
+                "loginU_var": username
+            }
+        )
+    else:    
+        print(f"377 Error : {err}") 
+        return templates.TemplateResponse(
+            "login.html", 
+            {
+                "request": request, 
+                "error_message": connection_result
+            }
+        )
+
+
+#===================================================================
+@app.post("/xlogin")
+async def process_login(
+        request: Request, 
+        username: str = Form(...), 
+        password: str = Form(...)
+        ):
+
+    #user = User("admin", "C1sco12345", "sandbox-iosxe-latest-1.cisco.com")
+    user = User(username, password, "sandbox-iosxe-latest-1.cisco.com")
+    cisco = user.cisco()
+    net_connect = NetConnection()
+    err = None
+    connection_result = net_connect.connect(cisco)  # Call the connect method with the object
+    userValid = connection_result is None or False
+            
+    print(f"360: {userValid}")
+    print(f"361: {connection_result}")
+
+
+    if userValid == True:
+
+        # Add the logged-in user to UsersLoggedIn dictionary
+        UsersLoggedIn[username] = TokenAccount(username, password)
+
+        # Print all account instances
+        for account_instance in TokenAccount.instances.values():
+            print(f"73=======: {account_instance.username} {account_instance.password}")
+
+            loginUsr = f"Account {account_instance.username} is accessing to Login... +"
+            print(f"+  {loginUsr}")
+            print(f"Number of User Logged-in to the System : {len(UsersLoggedIn)}")
+
+        account_instance = UsersLoggedIn.get(username)
+        if account_instance:
+            password = account_instance.password
+            print(f"368 =======: {username} {password}")
+            print(f"User :{username} STATUS: LOGGED IN")
+            
+        else:
+            print("Account not found.")
+
+
+        print(f"374 -- Error : {err}") 
+
+        return templates.TemplateResponse(
+            "search.html", 
+            {   "request": request, 
+                "loginU_var": username
+            }
+        )
+    else:    
+        print(f"377 Error : {err}") 
+        return templates.TemplateResponse(
+            "login.html", 
+            {
+                "request": request, 
+                "error_message": connection_result
+            }
+        )
+
+
+
+#===================================================================
+@app.post("/search")
+async def search(
+        request: Request, 
+        station: str = Form(...), 
+        loginU_var: str = Form(...)
+        ):
+
+    # Connect to the SQLite database
+    results = get_mapping_results(station)
+    resultsVLAN = get_vlan_results()
+    resultsVoice = get_voice_results()
+
+    print(results[0])
+
+    #view_modal = modal_selectedRow(idrow,station,port,interface,floor,location)
+
+    # Pass the results, search input, and loginU_var to the template
+    if len(UsersLoggedIn) != 0:
+        return templates.TemplateResponse(
+            "search.html",
+            {   "request": request, 
+                "results": results, 
+                "station": station, 
+                "loginU_var": loginU_var,
+                "resultsVLAN": resultsVLAN,
+                "resultsVoice": resultsVoice, 
+                #"viewModal": viewModal
+            }
+        )
+    else:
+        print("You must login first.")
+        return templates.TemplateResponse("login.html", {"request": request})
+
+
+#===================================================================
+@app.post("/process_modal_form1")
+async def process_modal_form(
+                request: Request, 
+                loginU_var: str = Form(...),   
+                idrow: str = Form(...),
+                floor: str = Form(...),
+                station: str = Form(...),                           
+                port: str = Form(...), 
+                interface: str = Form(...),
+                location: str = Form(...)
+                ):
+    
+
+    print("==========================================================")
+    print("Process: CLEAR PORT")
+    print("-----------------")
+    print("ID:", idrow)
+    print("Station:", station)
+    print("Port:", port)
+    print("Interface:", interface)
+    print("Floor:", floor)
+    print("Location:", location)
+    print("-----------------")
+    print("Clear Port Performed by :", loginU_var)
+    print("==========================================================")
+
+
+    username = loginU_var
+    account_instance = UsersLoggedIn.get(username)
+    if account_instance:
+        password = account_instance.password
+    else:
+        print("Account not found.")
+
+    print("--------------------------")
+    print("USER PASSWORD:", password)
+    print("--------------------------")
+
+
+    #result = login(username, password)
+    print(f"CLEAR PORT CODE HERE....")
+
+    
+    results = get_mapping_results(station)
+    resultsVLAN = get_vlan_results()
+    resultsVoice = get_voice_results()
+
+    # Process the form data as needed
+    return templates.TemplateResponse(
+        "search.html",
+        {   "request": request, 
+            "results": results, 
+            "idrow": idrow, 
+            "floor": floor, 
+            "station": station, 
+            "port": port, 
+            "interface": interface, 
+            "loginU_var": loginU_var,
+            "resultsVLAN": resultsVLAN,
+            "resultsVoice": resultsVoice
+
+        }
+
+    )
+
+
+
+
+#===================================================================
+@app.post("/process_modal_form2")
+async def process_modal_form(
+                request: Request,
+                loginU_var: str = Form(...),  
+                idrow: str = Form(...),
+                floor: str = Form(...),
+                station: str = Form(...),
+                port: str = Form(...), 
+                interface: str = Form(...),
+                location: str = Form(...),
+                VLANCustom: str = Form(...)
+                ):
+
+    print("==========================================================")
+    print("Process: Change VLAN")
+    print("-----------------")
+    print("ID:", idrow)
+    print("Station:", station)
+    print("Port:", port)
+    print("Interface:", interface)
+    print("Floor:", floor)
+    print("Location:", location)
+    print("Change to VLAN:", VLANCustom)
+    print("-----------------")
+    print("Change VLAN Performed by :", loginU_var)
+    print("==========================================================")
+    
+
+    username = loginU_var
+    if UsersLoggedIn.get(username):
+        password = UsersLoggedIn.get(username).password
+        #print(f"line116=======: {username} {password}")
+    else:
+        print("Account not found.")
+
+    results = get_mapping_results(station)
+    resultsVLAN = get_vlan_results()
+    resultsVoice = get_voice_results()
+
+    # Process the form data as needed
+
+    return templates.TemplateResponse(
+        "search.html",
+        {   "request": request, 
+            "results": results, 
+            "idrow": idrow, 
+            "floor": floor, 
+            "station": station, 
+            "port": port, 
+            "interface": interface, 
+            "loginU_var": loginU_var,
+            "resultsVLAN": resultsVLAN,
+            "resultsVoice": resultsVoice           
+        }
+    )
+
+
+
+
+
+#===================================================================
+@app.post("/process_modal_form3")
+async def process_modal_form(
+                request: Request,
+                loginU_var: str = Form(...),  
+                idrow: str = Form(...),
+                floor: str = Form(...),
+                station: str = Form(...),
+                port: str = Form(...), 
+                interface: str = Form(...),
+                VoiceCustom: str = Form(...)
+                ):
+
+
+
+    print("==========================================================")
+    print("Process: Change Voice")
+    print("-----------------")
+    print("ID:", idrow)
+    print("Station:", station)
+    print("Port:", port)
+    print("Interface:", interface)
+    print("Floor:", floor)
+    print("Location:", location)
+    print("Change to Voice:", VoiceCustom)
+    print("-----------------")
+    print("Change Voice Performed by :", loginU_var)
+    print("==========================================================")
+
+
+    username = loginU_var
+    if UsersLoggedIn.get(username):
+        password = UsersLoggedIn.get(username).password
+        print(f"line116=======: {username} {password}")
+    else:
+        print("Account not found.")
+
+    results = get_mapping_results(station)
+    resultsVLAN = get_vlan_results()
+    resultsVoice = get_voice_results()
+
+    # Process the form data as needed
+
+    return templates.TemplateResponse(
+        "search.html",
+        {   "request": request, 
+            "results": results, 
+            "idrow": idrow, 
+            "station": station, 
+            "port": port, 
+            "interface": interface, 
+            "loginU_var": loginU_var,
+            "resultsVLAN": resultsVLAN,
+            "resultsVoice": resultsVoice           
+        }
+    )
+
+
+
+
+
+#===================================================================
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8886)
+
+    #uvicorn.run(app, host="0.0.0.0", port=8886)
+#   uvicorn main:app --reload --host 0.0.0.0 --port 8886
+# arvin 7/15/2023
