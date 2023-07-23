3e36b25adbd566341ada436541925f8869c83860
arvinpenaacosta <117697006+arvinpenaacosta@users.noreply.github.com>
2023-07-23 19:15:11 +0800

    Update elogger.py

--- elogger.py
+++ elogger.py
@@ -4,34 +4,46 @@ import datetime
 def create_database():
     """Creates the database file if it does not exist."""
     try:
-        conn = sqlite3.connect('my_database.db')
+        conn = sqlite3.connect('elog.db')
     except sqlite3.Error as e:
         print(e)
         print('Creating database...')
-        conn = sqlite3.connect('my_database.db')
+        conn = sqlite3.connect('elog.db')
     return conn
 
 def create_table(conn):
     """Creates a table in the database."""
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS my_table (datestamp, indexrow, station, host, interface, floor, location, actions, doneby)')
+    c.execute('''CREATE TABLE IF NOT EXISTS eventlog (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    datestamp TEXT,
+                    indexrow INTEGER,
+                    station TEXT,
+                    host TEXT,
+                    interface TEXT,
+                    floor TEXT,
+                    location TEXT,
+                    actions TEXT,
+                    doneby TEXT
+                )''')
 
 def insert_data(conn, data):
     """Inserts data into the table."""
     c = conn.cursor()
-    c.execute('INSERT INTO my_table VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', data)
+    c.execute('INSERT INTO eventlog (datestamp, indexrow, station, host, interface, floor, location, actions, doneby) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', data)
     conn.commit()
 
 def main():
     conn = create_database()
     create_table(conn)
 
-    
+
     # Get the current date and time.
     now = datetime.datetime.now()
 
     # Convert the date and time to a string.
     datestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+
 
     # Sample data
     data = (datestamp, 1, 'StationA', 'HostA', 'InterfaceA', 'Floor1', 'LocationA', 'ActionA', 'UserA')
